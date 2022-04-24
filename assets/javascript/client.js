666b2c0d97042592c12d391634fac0057771d055
Devon <devon_3273@hotmail.com>
2022-04-24 16:04:45 -0600

    fixed toggle for all cards, edited footer placement

--- assets/javascript/client.js
+++ assets/javascript/client.js
@@ -6,16 +6,16 @@ const header = document.querySelector('header');
 const main = document.querySelector('.grid-container');
 const spotify = document.querySelector('.fa-spotify');
 const itunes = document.querySelector('.fa-itunes');
-const card = document.querySelector('.card');
+const cards = document.querySelectorAll('.card');
 
 
 // Dark mode toggle
 
-button.addEventListener('click', function(){
-nav.classList.toggle('darkmode');
-header.classList.toggle('darkmode');
-main.classList.toggle('darkmode');
-spotify.classList.toggle('darkmode');
-itunes.classList.toggle('darkmode');
-card.classList.toggle('darkmode')
+button.addEventListener('click', function () {
+  nav.classList.toggle('darkmode');
+  header.classList.toggle('darkmode');
+  main.classList.toggle('darkmode');
+  spotify.classList.toggle('darkmode');
+  itunes.classList.toggle('darkmode');
+  cards.forEach(card => card.classList.toggle('darkmode'))
 });
