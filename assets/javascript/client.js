8af53cf09106d46bdcd43acca9f8d90a8856322d
Devon <devon_3273@hotmail.com>
2022-04-24 16:12:36 -0600

    changed icons to match others

--- assets/javascript/client.js
+++ assets/javascript/client.js
@@ -4,9 +4,11 @@ const button = document.querySelector('.drk-btn');
 const nav = document.querySelector('nav');
 const header = document.querySelector('header');
 const main = document.querySelector('.grid-container');
-const spotify = document.querySelector('.fa-spotify');
-const itunes = document.querySelector('.fa-itunes');
+const spotify = document.querySelectorAll('.fa-spotify');
+const itunes = document.querySelectorAll('.fa-itunes');
 const cards = document.querySelectorAll('.card');
+const fb = document.querySelector('.fa-facebook-square');
+const pinterest = document.querySelector('.fa-pinterest');
 
 
 // Dark mode toggle
@@ -15,7 +17,9 @@ button.addEventListener('click', function () {
   nav.classList.toggle('darkmode');
   header.classList.toggle('darkmode');
   main.classList.toggle('darkmode');
-  spotify.classList.toggle('darkmode');
-  itunes.classList.toggle('darkmode');
-  cards.forEach(card => card.classList.toggle('darkmode'))
+  fb.classList.toggle('darkmode');
+  pinterest.classList.toggle('darkmode')
+  spotify.forEach(spotify => spotify.classList.toggle('darkmode'));
+  itunes.forEach(itunes => itunes.classList.toggle('darkmode'));
+  cards.forEach(card => card.classList.toggle('darkmode'));
 });
